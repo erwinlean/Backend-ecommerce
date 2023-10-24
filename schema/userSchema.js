d27ea1d06444e865bc853233ed390a99a0a988f0
Erwin Marte <erwin.mdq@gmail.com>
2023-10-23 16:38:22 -1000

    URI base64 default perfil image added

--- schema/userSchema.js
+++ schema/userSchema.js
@@ -1,42 +1,61 @@
-const mongoose = require ("mongoose");
-const encrypt = require ("bcrypt");
-const errMessage = require ("../errors/error-handler");
+"use strict";
+
+const mongoose = require('mongoose');
+const encrypt = require('bcrypt');
+const errMessage = require('../errors/error-handler');
+const fs = require('fs');
+const path = require('path');
+
+// Define the path to the default image
+const defaultImagePath = path.join(__dirname, '..', 'assets', 'users', 'default.png');
+// Read the default image file and convert it to a Base64 URI
+const defaultImageBase64 = fs.readFileSync(defaultImagePath, 'base64');
+const defaultImageURI = `data:image/png;base64,${defaultImageBase64}`
+
 const users = mongoose.Schema({
-    name:{
-        type:String,
-        min:4,
-        required:[true,errMessage.general.campo_obligatorio.minLegth]
-    }, 
-    email:{
-        type:String,
-        required:[true,errMessage.general.campo_obligatorio],
-        unique:true 
-    },
-    password:{
-        type:String,
-        required:[true,errMessage.general.campo_obligatorio],
-        min:7, 
-    },
-    userImg:{
+    name: {
         type: String,
-        default: "https://img.freepik.com/premium-vector/gray-avatar-icon-vector-illustration_276184-163.jpg"
+        min: 4,
+        required: [true, errMessage.general.campo_obligatorio.minLegth]
+    },
+    createDate: {
+        type: Date,
+        default: Date.now
+    },
+    email: {
+        type: String,
+        required: [true, errMessage.general.campo_obligatorio],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, errMessage.general.campo_obligatorio],
+        min: 7,
+    },
+    userImg: {
+        type: String,
+        default: defaultImageURI,
     }
 });
 
-users.pre("save",function(next){
-    if(this.password.includes(!Number&&String)){ 
-        console.log("El password debe tener numeros y letras.");
-    }else{
-        this.password=encrypt.hashSync(this.password,12);
-        console.log("password funcionando y encriptado");
+// Hash and password validation+email
+users.pre('save', function (next) {
+    if (!/^(?=.*[a-zA-Z])(?=.*\d).+$/.test(this.password)) {
+        return next(new Error('El password debe contener al menos una letra y un número.'));
+    } else {
+        this.password = encrypt.hashSync(this.password, 12);
         next();
-    } 
-    let emailCheck = users.email;
-    if (this.email){
-        emailCheck.includes("@"&&"."&&String); 
-    }else{
-        console.log("El mail no contiene @ y/o . necesarios.")
     }
-    next()
-})
-module.exports = mongoose.model("users",users);
+
+    let emailCheck = users.email;
+
+    if (this.email) {
+        emailCheck.includes('@' && '.' && String);
+    } else {
+        return next(new Error('El mail no contiene @ y/o . necesarios.'));
+    }
+
+    next();
+});
+
+module.exports = mongoose.model('users', users);
